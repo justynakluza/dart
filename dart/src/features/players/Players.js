0eecee293dd9e37cada0333b74b5043a574af489
justyna.kluza <justynakluza01@gmail.com>
2021-02-09 18:23:17 +0100

    add username limitations

--- dart/src/features/players/Players.js
+++ dart/src/features/players/Players.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addPlayer, selectPlayers, setActivePlayer, deletePlayer } from "../game/gameSlice";
+import {
+  addPlayer,
+  selectPlayers,
+  setActivePlayer,
+  deletePlayer
+} from "../game/gameSlice";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
@@ -8,26 +13,37 @@ export function Players() {
   const dispatch = useDispatch();
   const allPlayers = useSelector(selectPlayers);
   const [name, setName] = useState("");
-
+  const [placeholder, setPlaceholder] = useState("username");
 
   const addNewPlayer = () => {
     const newPlayer = {
       id: uuidv4(),
       scoreHistory: [],
       actualScore: 0,
-      name: name,
+      name: name
     };
-    dispatch(addPlayer(newPlayer));
-    setName("")
+    if (newPlayer.name.length === 0 ) {
+      setName("");
+      setPlaceholder("too short");
+
+    }
+    else if (newPlayer.name.length > 15) {
+      setName("");
+      setPlaceholder("too long");
+    }
+    else {
+      dispatch(addPlayer(newPlayer));
+      setName("");
+    }
   };
 
-  const makeActive = (e) => {
+  const makeActive = e => {
     dispatch(setActivePlayer(e.target.id));
   };
 
-  const deletePlayer2 = (e) => {
+  const deletePlayer2 = e => {
     dispatch(deletePlayer(e.target.id));
-  }
+  };
 
   return (
     <div>
@@ -37,15 +53,19 @@ export function Players() {
       <input
         value={name}
         type="text"
-        placeholder="username"
+        placeholder={placeholder}
         onChange={e => setName(e.target.value)}
       />
       <button onClick={addNewPlayer}>Add new player</button>
       {allPlayers.map((x, i) => (
         <div key={i}>
           <div>{x.name}</div>
-          <button id={x.id}onClick={makeActive}>is active</button>
-          <button id={x.id}onClick={deletePlayer2}>delete</button>
+          <button id={x.id} onClick={makeActive}>
+            is active
+          </button>
+          <button id={x.id} onClick={deletePlayer2}>
+            delete
+          </button>
         </div>
       ))}
       <br />
@@ -56,5 +76,3 @@ export function Players() {
     </div>
   );
 }
-
-//<button onClick={(e) => dispatch(addPlayer(e.target.value))}>Add next player</button>
